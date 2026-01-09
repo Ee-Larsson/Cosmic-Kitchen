54cd3e3abbc71535a734eb2937466ab43f3b524e
Hilda-Noteberg <Varge.noteberg@gmail.com>
2026-01-09 13:52:10 +0100

    Test fridge :D

---
+++ Cosmic Kitchen/Assets/scripts/Test_Fridge.cs
@@ -0,0 +1,24 @@
+using Assets.Scripts;
+using UnityEngine;
+
+public class Test_Fridge : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] Food food;
+    bool colliding;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        colliding = gameObject.GetComponent<ActionScript>().ReturnIfPlayerIsColiding();
+        if (colliding == true && Input.GetKeyDown(KeyCode.E))
+        {
+            player.GetComponent<Holding>().AddFood(food);
+        }
+    }
+}
