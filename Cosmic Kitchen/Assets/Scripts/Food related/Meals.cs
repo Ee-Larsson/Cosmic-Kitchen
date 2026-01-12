9997eb3116d7327e181922b97dcdca117ef1c7cb
Ee-Larsson <eemil.larsson@elev.ga.lbs.se>
2026-01-12 14:46:51 +0100

    order startup

--- Cosmic Kitchen/Assets/Scripts/Food related/Meals.cs
+++ Cosmic Kitchen/Assets/Scripts/Food related/Meals.cs
@@ -8,19 +8,22 @@ namespace Assets.Scripts.Food_related
 {
     public class Meals : Ingridents
     {
+        Random rand = new Random();
+
+
         //List of fruits
-        List<Food> fruits = new List<Food>();
+        List<Food> Fruits = new List<Food>();
         Food blapple = new Fruit("Blapple", true);
 
 
         //List of misc food items
         List<Food> general = new List<Food>();
-        Food bluegette = new Misc("Bluegette", false, false);
-
+        Food bluegette = new Misc("Bluegette", true, true);
+        Food omelette = new Misc("Omelette", true, false);
 
         //List of proper meals
         List<ProperFood> properMeals = new List<ProperFood>();
-        
+
 
         public Meals()
         {
@@ -28,12 +31,37 @@ namespace Assets.Scripts.Food_related
             fruits.Add(blapple);
 
             //Adds misc food items to list
-            miscs.Add(bluegette);
+            general.Add(bluegette);
+            general.Add(omelette);
 
             //Adds proper meals to list
             ProperFood MeatAndPotato = new ProperFood(null, null, (Meat)meats[0], (Misc)miscs[3]);
             properMeals.Add(MeatAndPotato);
 
+        }
+
+
+        public Food GetFruit()
+        {
+
+            int x = rand.Next(Fruits.Count);
+
+            return (Fruits[x]);
+        }
+
+        public Food GetMisc()
+        {
+
+            int x = rand.Next(general.Count);
+
+            return (general[x]);
+        }
+        public ProperFood GetProperMeals()
+        {
+
+            int x = rand.Next(properMeals.Count);
+
+            return (properMeals[x]);
         }
     }
 }
