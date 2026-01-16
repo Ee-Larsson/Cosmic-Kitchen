640a5342698a126deafdf5fdfe2bda72739f24de
Alma Björup <182209268+Koppar-AB@users.noreply.github.com>
2026-01-16 13:06:33 +0100

    StartSceneFinished!

--- Cosmic Kitchen/Assets/Scripts/CookingMinigame/ButtonScript.cs
+++ Cosmic Kitchen/Assets/Scripts/CookingMinigame/ButtonScript.cs
@@ -9,6 +9,8 @@ public class ButtonScript : MonoBehaviour
     public GameObject BAM;
     public GameObject fade;
 
+    Animator anim;
+
     Collider2D col;
     Rigidbody2D rb;
 
@@ -16,18 +18,24 @@ public class ButtonScript : MonoBehaviour
     void Start()
     {
         BAM.SetActive(false);
-        fade.SetActive(false);
+        //fade.SetActive(false);
 
         col = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
-        
+
+        fade.GetComponent<Animator>().Play("NoFadeAnim");
+        //anim = GetComponent<Animator>();
+
+        //anim.SetBool("isFading", false);
+
     }
 
     private void OnMouseDown()
     {
-        fade.SetActive(true);
-        //fade.GetComponent<Animator>().Play("FadeAnim");
         
+        //anim.SetBool("isFading", true);
+        fade.GetComponent<Animator>().Play("FadeAnim");
+
     }
 
     public void ChangeScene()
--- Cosmic Kitchen/Assets/Scripts/CookingMinigame/ChangeScene.cs
+++ Cosmic Kitchen/Assets/Scripts/CookingMinigame/ChangeScene.cs
@@ -5,24 +5,10 @@ using UnityEngine.UI;
 public class ChangeScene : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Image fade;
-    
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
 
     public void GoToSceneTestMap()
     {
-        fade.GetComponent<Animator>().Play("FadeAnim");
-        //SceneManager.LoadScene("TestMap");
+        print("CHANGING SCENE!");
+        SceneManager.LoadScene("TestMap");
     }
 }
