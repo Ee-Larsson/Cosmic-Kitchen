b3d5ada54b4c88fe201a1c7d0ec4100bfb63ec88
Hilda-Noteberg <Varge.noteberg@gmail.com>
2026-01-20 09:17:07 +0100

    Update Test_sink.cs

--- Cosmic Kitchen/Assets/scripts/Interaction things/Hildas test scripts/Test_sink.cs
+++ Cosmic Kitchen/Assets/scripts/Interaction things/Hildas test scripts/Test_sink.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class Test_sink : MonoBehaviour
 {
-    float cleaningTime = 2;
-    float currentCleaningTime = -1;
     void Update()
     {
         if (GetComponent<Holding>().ReturnHolding() != null)
