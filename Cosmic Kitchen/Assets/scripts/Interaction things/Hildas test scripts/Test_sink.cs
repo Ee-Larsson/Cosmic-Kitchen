a9352a3f728508e84ee5f458f3969029bdc26105
Hilda-Noteberg <Varge.noteberg@gmail.com>
2026-01-20 09:16:20 +0100

    removed my non functioning code

--- Cosmic Kitchen/Assets/scripts/Interaction things/Hildas test scripts/Test_sink.cs
+++ Cosmic Kitchen/Assets/scripts/Interaction things/Hildas test scripts/Test_sink.cs
@@ -9,17 +9,6 @@ public class Test_sink : MonoBehaviour
         if (GetComponent<Holding>().ReturnHolding() != null)
         {
             GetComponent<Holding>().PlaceFood();
-            GetComponent<ActionScript>().ReturnCollidingObject().GetComponent<Movement>().Cleaning();
-            currentCleaningTime = cleaningTime;
-            Debug.Log("blap");
-        }
-        if (currentCleaningTime >= 0)
-        {
-            currentCleaningTime -= Time.deltaTime;
-        }
-        if (currentCleaningTime <= 0)
-        {
-            GetComponent<ActionScript>().ReturnCollidingObject().GetComponent<Movement>().StopInteract();
         }
     }
 }
